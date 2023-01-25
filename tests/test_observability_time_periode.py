6605021abd464ef65e9676ec6aa2619efda4712e
Alexandre LEPETIT <10155402+aelttil@users.noreply.github.com>
2023-01-25 17:02:15 +0100

    Update test_observability_time_periode.py

--- tests/test_observability_time_periode.py
+++ tests/test_observability_time_periode.py
@@ -2,8 +2,8 @@ from libs.ShivaApi import Observability
 
 # Init clients
 shiva_obs_client = Observability(url="https://shiva.cloud-temple.com",
-                                   token_id="5a20ca1c-028c-46e8-8bea-6a0a0a6b8c7e",
-                                   token_secret="d27f838d-78f4-4873-ac4c-c98b68a995f4")
+                                   token_id="<changeme>",
+                                   token_secret="<changeme>")
 
 # Show all Appliances
 appliances = shiva_obs_client.get_appliances()
